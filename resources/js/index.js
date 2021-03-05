9472e98a487d864baae6e1f57e5517138b2b317b
nisrinasdr <sudrajat.nisrina@gmail.com>
2021-03-05 11:54:28 +0700

    change carousel final

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,4 @@
-const date = new Date('2021-03-05').getTime()
+const date = new Date('2021-09-05').getTime()
 
 const countdown = setInterval(() => {
     let today = new Date().getTime()
@@ -9,10 +9,18 @@ const countdown = setInterval(() => {
     let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-    document.getElementById('countDay').innerHTML = days + 'd'
-    document.getElementById('countHour').innerHTML = hours + 'h'
-    document.getElementById('countMinute').innerHTML = minutes + 'm'
-    document.getElementById('countSecond').innerHTML = seconds + 's'
+    if(days >= 0 && hours >= 0 && minutes >= 0 && seconds >= 0) {
+        document.getElementById('countDay').innerHTML = days + 'd'
+        document.getElementById('countHour').innerHTML = hours + 'h'
+        document.getElementById('countMinute').innerHTML = minutes + 'm'
+        document.getElementById('countSecond').innerHTML = seconds + 's'
+    } else {
+        document.getElementById('countDay').innerHTML = '0'
+        document.getElementById('countHour').innerHTML = '0'
+        document.getElementById('countMinute').innerHTML = '0'
+        document.getElementById('countSecond').innerHTML = '0'
+    }
+    
     
 }, 1000);
 
