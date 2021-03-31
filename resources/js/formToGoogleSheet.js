20b95d3fa1348809a1771c68beb74dd6f6b39be7
nisrinasdr <sudrajat.nisrina@gmail.com>
2021-03-31 11:20:08 +0700

    form to google docs and grid

---
+++ resources/js/formToGoogleSheet.js
@@ -0,0 +1,18 @@
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbyec3Dkj1H5F3A99H8DGxspxM5ZAPfNukhxfO0WlmBqZVhVzdSyp0ApnCsuADPoDAM/exec'
+// const form = document.forms['submit-to-google-sheet']
+
+// form.addEventListener('submit', e => {
+// e.preventDefault()
+// fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//     .then(response => {
+//         alert('Message sent!');
+        
+//     })
+//     .catch(error => console.error('Error!', error.message))
+    
+//     document.getElementById("nama").value = ""
+//     document.getElementById("kehadiran").value = ""
+//     document.getElementById("pesan").value = ""
+// })
+
+        
